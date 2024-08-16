f2a4c076a2d2bd2726b8c0eb4db7b00c4072ac71
knilix <90577576+knilix@users.noreply.github.com>
2024-08-16 18:32:29 +0200

    Update debianautoupdateinstallscript.sh

--- debianautoupdateinstallscript.sh
+++ debianautoupdateinstallscript.sh
@@ -7,13 +7,14 @@ mkdir -p /opt/scriptfiles
 touch /opt/scriptfiles/updatescript.sh
 touch /opt/scriptfiles/updatelog.txt
 #
+echo #!/bin/bash | tee -a /opt/scriptfiles/updatescript.sh
 echo apt-get update -y | tee -a /opt/scriptfiles/updatescript.sh
 echo apt-get upgrade -y | tee -a /opt/scriptfiles/updatescript.sh
-#
-echo d=$(date +%y-%m-%d_%H:%M:%S) | tee -a /opt/scriptfiles/updatelog.txt
+echo 'echo d=$(date +%y-%m-%d_%H:%M:%S) | tee -a /opt/scriptfiles/updatelog.txt' | tee -a /opt/scriptfiles/updatescript.sh
 #
 chmod 700 /opt/scriptfiles/updatescript.sh
 #
 cd && cd /opt/scriptfiles
 ./updatescript.sh
 #
+rm -r /opt/scriptfiles/testarea-main
