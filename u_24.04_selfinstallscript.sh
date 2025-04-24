4f14659de97d256b9df3559cbe8a09a9c2a89786
knilix <90577576+knilix@users.noreply.github.com>
2025-04-24 15:23:38 +0200

    Update u_24.04_selfinstallscript.sh

--- u_24.04_selfinstallscript.sh
+++ u_24.04_selfinstallscript.sh
@@ -10,21 +10,26 @@ sudo apt-get install abiword -y
 sudo apt-get install blender -y
 sudo apt-get install kdenlive -y
 sudo apt-get install plasma-workspace-wayland -y
-
-
-
-
-
-
-
-sudo apt-get install flatpak -y
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# Wine
+sudo dpkg --add-architecture i386
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key
+sudo wget -NP /etc/apt/sources.list.d/ "https://dl.winehq.org/wine-builds/ubuntu/dists/$(lsb_release -c | grep -o '\w*$')/winehq-$(lsb_release -c | grep -o '\w*$').sources"
+sudo apt-get update
+sudo apt-get install --install-recommends winehq-devel
+# Lutris
+echo "deb [signed-by=/etc/apt/keyrings/lutris.gpg] https://download.opensuse.org/repositories/home:/strycore/Debian_12/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list > /dev/null
+wget -q -O- https://download.opensuse.org/repositories/home:/strycore/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/keyrings/lutris.gpg > /dev/null
+sudo apt-get update
+sudo apt-get install lutris -y
+# Snap-Packete
 sudo snap install musicpod
 sudo snap install blue-recorder
 sudo snap install discord
 sudo snap install obs-studio
-
-
+# Flatpak
+sudo apt-get install flatpak -y
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 #
 rm -r /opt/scriptfiles/testarea-main
 rm /opt/main.zip
@@ -34,4 +39,5 @@ echo #
 echo #
 echo "- Das Script wurde ausgeführt"
 echo "- Nicht mehr benötigte Installationsdateien wieder gelöscht"
+echo "- System jetzt neu starten!"
 echo #
