b3d753f7934428a05f03787540bfea79e071553d
knilix <90577576+knilix@users.noreply.github.com>
2025-04-26 15:58:53 +0200

    Update flatpak-installs.sh

--- flatpak-installs.sh
+++ flatpak-installs.sh
@@ -2,14 +2,12 @@
 # Maintener: @knilix
 # Only test
 #
-#sudo apt install flatpak -y
-#flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-#
+# sudo apt install flatpak -y
+# flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# root user required (su)
 # Now the script
 # Startscript: wget -q -P /opt/ https://github.com/knilix/testarea/archive/refs/heads/main.zip && unzip /opt/main.zip -d /opt/scriptfiles && chmod 700 /opt/scriptfiles/testarea-main/flatpak-installs.sh
 # Ausführbefehl (einmalig): cd /opt/scriptfiles/testarea-main && ./flatpak-installs.sh
-
-#flatpak install flathub org.gimp.GIMP flathub com.abisource.AbiWord flathub org.blender.Blender flathub net.lutris.Lutris flathub org.kde.kdenlive flathub com.obsproject.Studio flathub org.feichtmeier.Musicpod flathub sa.sy.bluerecorder flathub org.flameshot.Flameshot flathub com.usebottles.bottles
 #
 flatpak install -y \
   flathub org.gimp.GIMP \
