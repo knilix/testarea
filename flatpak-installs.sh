7fff07187e71a46496210aa7f265aa2646f2f798
knilix <90577576+knilix@users.noreply.github.com>
2025-04-27 14:10:29 +0200

    Update flatpak-installs.sh

--- flatpak-installs.sh
+++ flatpak-installs.sh
@@ -9,7 +9,7 @@
 # Vorher erledigen: 
 # - installieren von wget und zip 
 #
-# Es werden mit diesem Script Flatpak und folgende Apps installiert: Discord, Ginp, AbiWord, Blender, Lutris, Kdenlive, OBSStudio, Misicpod, BlueRecorder, Flameshot, Bottles, ProtonPlus, BoxBuddy, 
+# Es werden mit diesem Script Flatpak und folgende Apps installiert: Discord, Gimp, AbiWord, Blender, Lutris, Kdenlive, OBSStudio, Misicpod, BlueRecorder, Flameshot, Bottles, ProtonPlus, BoxBuddy, 
 # Clapgrep, Filelight, Flatseal, Gearlever, Gimp, Photoolibre, Protontricks, Warehouse, WineZGUI.
 # Es wird geprüft, ob Flatpak installiert ist. Wenn nicht, wird es installiert.
 # Es wird geprüft of die zu installierenden Flatpak-Apps schon per Snap oder Nativ installiert sind. Wenn ja, wird die Installation der jeweiligen Flatpak-App übersprungen.
@@ -21,7 +21,7 @@
 # Do it beforehand:
 # - install wget and zip
 #
-# This script installs Flatpak and the following apps: Discord, Ginp, AbiWord, Blender, Lutris, Kdenlive, OBSStudio, Misicpod, BlueRecorder, Flameshot, Bottles, ProtonPlus, BoxBuddy, Clapgrep, Filelight,
+# This script installs Flatpak and the following apps: Discord, Gimp, AbiWord, Blender, Lutris, Kdenlive, OBSStudio, Misicpod, BlueRecorder, Flameshot, Bottles, ProtonPlus, BoxBuddy, Clapgrep, Filelight,
 # Flatseal, Gearlever, Gimp, Photoolibre, Protontricks, Warehouse, WineZGUI.
 # The system checks whether Flatpak is installed. If not, it is installed.
 # The system checks whether the Flatpak apps to be installed are already installed via Snap or Native. If yes, the installation of the respective Flatpak app is skipped.
