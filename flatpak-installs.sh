8e232ffe2813dba0063be9fa0fd5d8113fc982b7
knilix <90577576+knilix@users.noreply.github.com>
2025-04-28 20:53:29 +0200

    Update flatpak-installs.sh

--- flatpak-installs.sh
+++ flatpak-installs.sh
@@ -10,7 +10,7 @@
 # - installieren von wget und zip 
 #
 # Es werden mit diesem Script Flatpak und folgende Apps installiert: Discord, Gimp, AbiWord, Blender, Lutris, Kdenlive, OBSStudio, Misicpod, BlueRecorder, Flameshot, Bottles, ProtonPlus, BoxBuddy, 
-# Clapgrep, Filelight, Flatseal, Gearlever, Gimp, Photoolibre, Protontricks, Warehouse, WineZGUI.
+# Clapgrep, Filelight, Flatseal, Gearlever, Gimp, Photoolibre, Protontricks, Warehouse, WineZGUI, NotepadNext.
 # Es wird geprüft, ob Flatpak installiert ist. Wenn nicht, wird es installiert.
 # Es wird geprüft of die zu installierenden Flatpak-Apps schon per Snap oder Nativ installiert sind. Wenn ja, wird die Installation der jeweiligen Flatpak-App übersprungen.
 # Warnhinweise, die nur informativ sind und keinerlei Einfluss auf die Funktion der jeweiligen App haben, z.B. weil KDE statt Gnome verwendet wird, werden ausgeblendet.
@@ -22,7 +22,7 @@
 # - install wget and zip
 #
 # This script installs Flatpak and the following apps: Discord, Gimp, AbiWord, Blender, Lutris, Kdenlive, OBSStudio, Misicpod, BlueRecorder, Flameshot, Bottles, ProtonPlus, BoxBuddy, Clapgrep, Filelight,
-# Flatseal, Gearlever, Gimp, Photoolibre, Protontricks, Warehouse, WineZGUI.
+# Flatseal, Gearlever, Gimp, Photoolibre, Protontricks, Warehouse, WineZGUI, NotepadNext.
 # The system checks whether Flatpak is installed. If not, it is installed.
 # The system checks whether the Flatpak apps to be installed are already installed via Snap or Native. If yes, the installation of the respective Flatpak app is skipped.
 # Warnings that are only informative and have no influence on the function of the respective app, e.g. because KDE is used instead of Gnome, are hidden.
@@ -142,6 +142,7 @@ FLATPAK_APPS=(
   "com.github.Matoking.protontricks"
   "io.github.flattool.Warehouse"
   "io.github.fastrizwaan.WineZGUI"
+  "com.github.dail8859.NotepadNext"
 )
 
 for app in "${FLATPAK_APPS[@]}"; do
