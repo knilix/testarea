363783edc13ab3baee9c4991a9f008cb427003b8
knilix <90577576+knilix@users.noreply.github.com>
2025-05-04 16:09:51 +0200

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -433,6 +433,9 @@ rm /var/www/nextcloud/data/nextcloud.log
 # cron.php einmalig anschieben
 sudo -u www-data php /var/www/nextcloud/cron.php >/dev/null 2>&1
 
+# Zusätzliche Anfrage an den Server senden, um die Sitzung zu initialisieren
+curl -s -o /dev/null http://localhost
+
 sudo -u www-data php /var/www/nextcloud/occ maintenance:mode --off
 #######################################################################################################################################################################
 
