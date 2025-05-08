1e897c1eaf44f9d9b65940c47d52d219abfa16e0
knilix <90577576+knilix@users.noreply.github.com>
2025-05-08 19:37:24 +0200

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -2,6 +2,11 @@
 # Maintainer: @knilix
 # Version: 1.0
 # Hinweis: Für Debian 12 und Ubuntu ab 22.04+ (x64), root erforderlich
+#
+# Herunterladen: wget -q -P /opt/ https://github.com/knilix/testarea/archive/refs/heads/main.zip && unzip /opt/main.zip -d /opt/scriptfiles && chmod 700 /opt/scriptfiles/testarea-main/nextcloud.sh
+# Installieren: cd && cd /opt/scriptfiles/testarea-main && ./nextcloud.sh
+# Bei Problemen, das Heuntergeladene wieder löschen: rm -rf /opt/scriptfiles/testarea-main /opt/main.zip 
+#
 # Script nur einmalig ausführen - - Abfrage einer vorhandenen Nextcloud-Datenbank noch nicht implementiert!
 # Mit MariaDB und Redis Cache
 # -----------------------------------------------------------------------------
