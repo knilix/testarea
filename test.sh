2d37e83b5908a527a1edcd9baba0fdda2f6f1db4
knilix <90577576+knilix@users.noreply.github.com>
2025-05-09 15:15:08 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Maintainer: @knilix
 # Version: 1.1
-# Hinweis: Für Debian 12 und Ubuntu ab 22.04+ (x64), root erforderlich
+# Hinweis: Für Ubuntu ab 22.04+ (x64), root erforderlich
 #
 # Herunterladen: wget -q -P /opt/ https://github.com/knilix/testarea/archive/refs/heads/main.zip && unzip /opt/main.zip -d /opt/scriptfiles && chmod 700 /opt/scriptfiles/testarea-main/test.sh
 # Installieren: cd && cd /opt/scriptfiles/testarea-main && ./test.sh
