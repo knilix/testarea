96b0253abc7382e0ad9329d1c2eb1decddddceed
knilix <90577576+knilix@users.noreply.github.com>
2025-05-09 10:26:58 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@
 # Version: 1.0
 # Hinweis: Für Debian 12 und Ubuntu ab 22.04+ (x64), root erforderlich
 #
-# Herunterladen: wget -q -P /opt/ https://github.com/knilix/testarea/archive/refs/heads/main.zip && unzip /opt/main.zip -d /opt/scriptfiles && chmod 700 /opt/scriptfiles/testarea-main/nextcloud.sh
-# Installieren: cd && cd /opt/scriptfiles/testarea-main && ./nextcloud.sh
+# Herunterladen: wget -q -P /opt/ https://github.com/knilix/testarea/archive/refs/heads/main.zip && unzip /opt/main.zip -d /opt/scriptfiles && chmod 700 /opt/scriptfiles/testarea-main/test.sh
+# Installieren: cd && cd /opt/scriptfiles/testarea-main && ./test.sh
 # Bei Problemen, das Heuntergeladene wieder löschen: rm -rf /opt/scriptfiles/testarea-main /opt/main.zip 
 #
 # Script nur einmalig ausführen - - Abfrage einer vorhandenen Nextcloud-Datenbank noch nicht implementiert!
